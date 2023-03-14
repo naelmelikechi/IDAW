64daa5b3b990ab8b50dd4d08c1af7d010787cb74
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-14 10:12:35 +0100

    template footer

--- SitePro/v2/cv.php
+++ SitePro/v2/cv.php
@@ -34,10 +34,5 @@
       </div>
     </div>
   </div>
-  <footer class="card-footer fixed-bottom">
-    <p class="row justify-content-center">Site web réalisé dans le cadre de l'UV IDAW à l'IMT Nord Europe par&nbsp<a
-        href="mailto:nael.melikechi@etu.imt-nord-europe.fr"> Nael Melikechi</a></p>
-  </footer>
-</body>
 
-</html>
+  <?php require_once('template_footer.php');?>
---
+++ SitePro/v2/template_footer.php
@@ -0,0 +1,7 @@
+<footer class="card-footer fixed-bottom">
+    <p class="row justify-content-center">Site web réalisé dans le cadre de l'UV IDAW à l'IMT Nord Europe par&nbsp<a
+        href="mailto:nael.melikechi@etu.imt-nord-europe.fr"> Nael Melikechi</a></p>
+  </footer>
+</body>
+
+</html>
