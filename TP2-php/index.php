7f536f03a026d8ddf60a4468e27bde3823b32588
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-14 09:00:53 +0100

    PHP HEURE

---
+++ TP2-php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Page d'accueil</title>
+</head>
+
+<body>
+    <h1>Heure actuelle :</h1>
+    <p>
+        <?php echo date('H:i:s'); ?>
+    </p>
+</body>
+
+
+</html>
