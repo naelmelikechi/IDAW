93e1b9eb56e0f00da48be44924e1bce862504b27
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-14 10:03:46 +0100

    initialisation sitepro v2

---
+++ SitePro/v2/projets.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Nael Melikechi</title>
+  <link rel="stylesheet" href="bootstrap-dist/css/bootstrap.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="bootstrap-dist/js/bootstrap.js"></script>
+</head>
+
+<body>
+  <header class="bg-light">
+    <div class="container">
+      <div class="row justify-content-center">
+        <h1>Nael Melikechi</h1>
+      </div>
+    </div>
+
+  </header>
+  <div class="text-right  mr-5 pr-3">
+    <img src="files/images/avatar.jpg" alt="avatar" class="img-thumbnail" style="max-width:5%">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+
+        <nav class="nav nav-pills nav-fill">
+          <div class="nav flex-column nav-pills">
+            <a href="index.html" class="nav-link">Accueil</a>
+            <a href="cv.html" class="nav-link">CV</a>
+            <a class="nav-link active" href="projets.html" role="tab" aria-selected="true">Projets</a>
+          </div>
+        </nav>
+      </div>
+
+      <div class="col grow">
+        <article>
+          <h2>Voici les differents projets que j'ai réalisé</h2>
+          <p>Voici mon lien <a href="https://github.com/naelmelikechi">GitHub</a>.</p>
+        </article>
+      </div>
+    </div>
+  </div>
+  <footer class="card-footer fixed-bottom">
+    <p class="row justify-content-center">Site web réalisé dans le cadre de l'UV IDAW à l'IMT Nord Europe par&nbsp<a
+        href="mailto:nael.melikechi@etu.imt-nord-europe.fr"> Nael Melikechi</a></p>
+  </footer>
+</body>
+
+</html>
