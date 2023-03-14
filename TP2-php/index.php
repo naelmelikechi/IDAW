e0784b294b5ae86b4dec335014bfb901d72c2c9b
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-14 10:01:57 +0100

    php fonctions test

--- TP2-php/index.php
+++ TP2-php/index.php
@@ -9,7 +9,32 @@
 <body>
     <h1>Heure actuelle :</h1>
     <p>
-        <?php echo date('H:i:s'); ?>
+        <?php echo date('H:i:s');?>
+    </p>
+    <p>
+        <?php
+        echo date('d:m:Y');?>
+    </p>
+    <p>
+        <?php
+            $tab1 = array("A","B","C");
+            $tab2[0] = "A";
+            $tab2[1] = "B";
+            $tab2[2] = "C";
+            $tab3[] = "A";
+            $tab3[] = "B";
+            $tab3[] = "C";            
+        ?>
+    </p>
+    <p>
+        <?php
+            function helloWorld($var1, $var2) {
+                echo "Hello, World! var1 = " . $var1 . " var2 = " . $var2;
+            }
+            
+            helloWorld("a","b");
+        ?>
+
     </p>
 </body>
 
