0779f482ce9c8a02915915a85184a44c7c8471a4
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-17 09:33:31 +0100

    exercice1

--- TP3-PHP/1-Form_GET/connected.php
+++ TP3-PHP/1-Form_GET/connected.php
@@ -1,10 +1,37 @@
 <?php
-if (isset($_GET['login']) && isset($_GET['password'])) {
-    $login = $_GET['login'];
-    $password = $_GET['password'];
-    echo "<p>Login : $login.</p>";
-    echo "<p>Mot de passe : $password.</p>";
+/*if (isset($_GET['login']) && isset($_GET['password'])) {
+$login = $_GET['login'];
+$password = $_GET['password'];
+echo "<p>Login : $login.</p>";
+echo "<p>Mot de passe : $password.</p>";
 } else {
-    echo "<p>Login et Password non définis</p>";
+echo "<p>Login et Password non définis</p>";
+}*/
+
+// on simule une base de données
+$users = array(
+    // login => password
+    'riri' => 'fifi',
+    'yoda' => 'maitrejedi'
+);
+$login = "anonymous";
+$errorText = "";
+$successfullyLogged = false;
+if (isset($_GET['login']) && isset($_GET['password'])) {
+    $tryLogin = $_GET['login'];
+    $tryPwd = $_GET['password'];
+    // si login existe et password correspond
+    if (array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
+        $successfullyLogged = true;
+        $login = $tryLogin;
+    } else
+        $errorText = "Erreur de login/password";
+} else
+    $errorText = "Merci d'utiliser le formulaire de login";
+if (!$successfullyLogged) {
+    echo $errorText;
+} else {
+    echo "<h1>Bienvenue" . $login . "</h1>";
 }
+
 ?>
