f8030ddec8f71e16a9abe3722709ef9f331d576c
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-20 15:20:52 +0100

    commit

--- TP4-PHP-REST/PART1/insertDB.sql
+++ TP4-PHP-REST/PART1/insertDB.sql
@@ -2,6 +2,5 @@ INSERT INTO `users` (`id`, `name`, `email`) VALUES
 (1, 'riri', 'riri@disney.com'),
 (2, 'fifi', 'fifi@disney.com'),
 (3, 'loulou', 'loulou@disney.com'),
-(4, 'nael', 'nael@gmail.com'),
-(5, 'aaaaaaa', 'aaaa@bb.com');
+(4, 'nael', 'nael@gmail.com');
 COMMIT;
--- TP4-PHP-REST/PART2/index.php
+++ TP4-PHP-REST/PART2/index.php
@@ -2,9 +2,20 @@
 require_once('initPDO.php');
 require_once('db_init.php');
 
+if (isset($_POST['name']) && isset($_POST['email'])) {
+
+    $insert = $pdo->prepare("INSERT INTO users ( name, email) VALUES (:name,:email)");
+    $insert->bindParam(':name', $name);
+    $insert->bindParam(':email', $email);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $insert->execute();
+}
+
 $request = $pdo->prepare("select * from users");
 $request->execute();
 $users = $request->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,16 +26,41 @@ $users = $request->fetchAll(PDO::FETCH_OBJ);
 </head>
 
 <body>
-<?php
-echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-foreach ($users as $user) {
-    echo "<tr><td>" . $user->id . "</td><td>" . $user->name . "</td><td>" . $user->email . "</td></tr>";
-}
-echo "</table>";
+    <h1>Liste des utilisateurs</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+        <?php
+        foreach ($users as $user) {
+            echo "<tr><td>" . $user->id . "</td><td>" . $user->name . "</td><td>" . $user->email . "</td></tr>";
+        }
+        ?>
+    </table>
+    <h2>Ajouter un utilisateur :</h2>
+    <form id="add_user" action="" method="POST">
+        <table>
+            <tr>
+                <th>Nom :</th>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <th>Mot de passe :</th>
+                <td><input type="text" name="email"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="Ajouter Utilisateur" /></td>
+            </tr>
+        </table>
+    </form>
 
-$pdo = NULL;
+    <?php
+    $pdo = NULL;
 
-?>
+    ?>
 
 </body>
 
