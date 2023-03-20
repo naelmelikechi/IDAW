d8375bde373abaa8bd846658b3f2c7c0b6ba4b8c
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-20 11:48:22 +0100

    modif dossiers

---
+++ TP4-PHP-REST/PART2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>USERS</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+
+    <?php
+
+    require_once('initPDO.php');
+    require_once('db_init.php');
+
+    $request = $pdo->prepare("select * from users");
+    $request->execute();
+    $users = $request->fetchAll(PDO::FETCH_OBJ);
+
+    echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+    foreach ($users as $user) {
+        echo "<tr><td>" . $user->id . "</td><td>" . $user->name . "</td><td>" . $user->email . "</td></tr>";
+    }
+    echo "</table>";
+
+    $pdo = null;
+
+    ?>
+
+</body>
+
+</html>
