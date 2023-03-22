ce0a376036086a89aa53bde5b87168a69e2c501d
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-22 11:53:53 +0100

    update ne fonctionne pas encore

--- TP4-PHP-REST/Exercice5/api.php
+++ TP4-PHP-REST/Exercice5/api.php
@@ -4,7 +4,56 @@ require_once('users.php');
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'GET') {
-    $users = getAllUsers();
-    echo json_encode($users);
+    if (isset($_GET['id'])) {
+        $user = getUserById($_GET['id']);
+        if ($user) {
+            echo json_encode($user);
+        } else {
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(['error' => 'User not found']);
+        }
+    } else {
+        $users = getAllUsers();
+        echo json_encode($users);
+    }
+}
+
+if ($method == 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $user = createUser($data['name'], $data['email']);
+    if ($user) {
+        header('HTTP/1.1 201 Created');
+        echo json_encode($user);
+    } else {
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(['error' => 'Could not create user']);
+    }
+}
+
+if ($method == 'DELETE') {
+    if (isset($_GET['id'])) {
+        $result = deleteUserById($_GET['id']);
+        if ($result) {
+            header('HTTP/1.1 204 No Content');
+        } else {
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(['error' => 'User not found']);
+        }
+    } else {
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(['error' => 'Missing ID parameter']);
+    }
+}
+
+if ($method == 'PUT') {
+    parse_str(file_get_contents("php://input"),$post_vars);
+    $data = json_decode($post_vars['data'], true);
+    $result = updateUserById($_GET['id'], $data['name'], $data['email']);
+    if ($result) {
+        echo json_encode(['success' => 'User updated successfully']);
+    } else {
+        header('HTTP/1.1 404 Not Found');
+        echo json_encode(['error' => 'User not found']);
+    }
 }
 ?>
