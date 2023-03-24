5d5fe4ba1d275ea098c878cc01df395b009630b5
naelmelikechi <naelmelikechi7@gmail.com>
2023-03-24 14:11:37 +0100

    erreur

--- TP5/exo2/insertDB.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `users` (`id`, `name`, `email`) VALUES
-(1, 'Aaliyah', 'aaliyah@yahoo.com'),
-(2, 'Frank Ocean', 'frank@blonded.com'),
-(3, 'Khali', 'khali@palmer.com'),
-(4, '070Shake', 'dani@gmail.com'),
-(5, 'Laylow', 'laylow@digital.com');
-COMMIT;
