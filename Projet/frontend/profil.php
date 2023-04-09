c4c2957601a90f679cc7c7cc056c05ec2b91e10b
Arnaud Rush <arnaud.rushenas@gmail.com>
2023-04-09 17:51:23 +0200

    profil

--- Projet/frontend/profil.php
+++ Projet/frontend/profil.php
@@ -1,9 +1,16 @@
 <?php
+session_start();
 require_once 'templates/template_header.php';
 require_once 'templates/template_menu.php';
+require_once 'config.php';
 
+if (!isset($_SESSION['ID_UTILISATEUR'])) {
+    header("Location: ../login.php");
+    exit;
+}
+
+$user_id = $_SESSION['ID_UTILISATEUR'];
 
 $currentPage = 'profil';
 renderMenuToHTML($currentPage);
-
 ?>
