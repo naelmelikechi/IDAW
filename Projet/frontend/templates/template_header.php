8aa4a89ea4aefb7acaae426fca440a73146322d3
Arnaud Rush <arnaud.rushenas@gmail.com>
2023-04-09 18:08:22 +0200

    ajout chart.js

--- Projet/frontend/templates/template_header.php
+++ Projet/frontend/templates/template_header.php
@@ -4,6 +4,7 @@
         <meta charset='utf-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <link rel="stylesheet" href="css/styles.css">
         <title>iMangerMieux</title>
     </head>
