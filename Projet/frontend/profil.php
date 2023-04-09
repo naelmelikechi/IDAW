52f096115b3901947f4d4e91e05a6057c391dba4
Arnaud Rush <arnaud.rushenas@gmail.com>
2023-04-09 18:00:26 +0200

    profil données

--- Projet/frontend/profil.php
+++ Projet/frontend/profil.php
@@ -14,3 +14,49 @@ $user_id = $_SESSION['ID_UTILISATEUR'];
 $currentPage = 'profil';
 renderMenuToHTML($currentPage);
 ?>
+
+<body>
+    <header>
+        <h2 id="profile-title">Profil utilisateur</h2>
+    </header>
+
+    <section id="user-info">
+        <h3>Informations personnelles</h3>
+        <div id="user-details">
+            <p>Nom : <span id="user-nom"></span></p>
+            <p>Prénom : <span id="user-prenom"></span></p>
+            <p>Email : <span id="user-email"></span></p>
+            <p>Age : <span id="user-age"></span></p>
+            <p>Poids : <span id="user-poids"></span></p>
+            <p>Sexe : <span id="user-sexe"></span></p>
+            <p>Niveau d'activité sportive : <span id="user-niveau_activite_sportive"></span></p>
+        </div>
+    </section>
+
+    <script>
+        let api_url = "<?php echo $API_LINK ?>";
+        let userId = <?php echo $user_id ?>;
+
+        function getUserByID() {
+            $.ajax({
+                url: api_url + '/utilisateurs?id=' + userId,
+                type: 'GET',
+                dataType: 'json',
+            }).done(function (response) {
+                $('#user-nom').text(response.NOM);
+                $('#user-prenom').text(response.PRENOM);
+                $('#user-email').text(response.EMAIL);
+                $('#user-age').text(response.AGE);
+                $('#user-poids').text(response.POIDS);
+                $('#user-sexe').text(response.SEXE);
+                $('#user-niveau_activite_sportive').text(response.NIVEAU_ACTIVITE_SPORTIVE);
+            }).fail(function (error) {
+                alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+            });
+        }
+
+        getUserByID();
+    </script>
+</body>
+
+</html>
